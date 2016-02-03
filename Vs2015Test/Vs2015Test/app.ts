8d99a6c40cc9fb86aaf212123bd1fae1636cdc55
snerks <martin.github@outlook.com>
2016-02-03 22:54:16 +0000

    Initial commit

---
+++ Vs2015Test/Vs2015Test/app.ts
@@ -0,0 +1,28 @@
+﻿class Greeter {
+    element: HTMLElement;
+    span: HTMLElement;
+    timerToken: number;
+
+    constructor(element: HTMLElement) {
+        this.element = element;
+        this.element.innerHTML += "The time is: ";
+        this.span = document.createElement('span');
+        this.element.appendChild(this.span);
+        this.span.innerText = new Date().toUTCString();
+    }
+
+    start(): void {
+        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
+    }
+
+    stop(): void {
+        clearTimeout(this.timerToken);
+    }
+
+}
+
+window.onload = () => {
+    const el = document.getElementById('content');
+    const greeter = new Greeter(el);
+    greeter.start();
+};
